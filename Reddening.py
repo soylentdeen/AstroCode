dcee289b14a18417f7639e73ffa62bcf391c9f64
Casey <deen@ramen.(none)>
2011-03-02 01:54:09 -0600

    Started Spectral Dereddening

--- Reddening.py
+++ Reddening.py
@@ -29,3 +29,28 @@ def cttReddening(j, dj, h, dh, k, dk, **kwargs):
     dAj = numpy.sqrt((dj/(denom*(Mred-Mctts)))**2.0+(dh*(1.0+Mctts)/(denom*(Mred-Mctts)))**2.0+(dk*(Mctts/(denom*(Mred-Mctts))))**2.0)
 
     return (Aj, dAj)
+
+
+def spectralReddening(wl, flux, dFlux, spt, **kwargs):
+    xJ = 1.235
+
+    if ( 'beta' in kwargs):
+        beta = kwargs['beta']
+    else:
+        beta = -1.96
+    
+    wlStart = 1.1   # microns
+    wlStop = 1.3    # microns
+
+    strongLines = [1.1789, 1.1843, 1.1896, 1.1995, 1.282]
+    lineWidths = [0.002, 0.002, 0.002, 0.003, 0.005]
+
+    bm = scipy.where( (wl > wlStart) & (wl < wlStop) )[0]
+    for line, width in zip(strongLines, lineWidths):
+        new_bm = scipy.where( abs(wl[bm]-line) > width)
+        bm = bm[new_bm[0]]
+
+    x = wl[bm]
+    y = flux[bm]
+    dy = dFlux[bm]
+
