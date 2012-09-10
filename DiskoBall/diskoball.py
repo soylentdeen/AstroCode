6f815fee3f27a19f902040136030ee85a4942c44
Casey Deen <deen@mpia.de>
2012-09-10 05:24:09 +0200

    subsampled diskint

--- DiskoBall/diskoball.py
+++ DiskoBall/diskoball.py
@@ -18,13 +18,24 @@ pi = numpy.pi
 total_surface_area = 4.0*pi
 cell_area = total_surface_area/ncells
 
-dfI = '/home/deen/Data/MoogStokes/Verification_Data/testline/delo.spectrum_I'
-dfContinuum = '/home/deen/Data/MoogStokes/Verification_Data/testline/delo.continuum'
-dfAngles = '/home/deen/Data/MoogStokes/Verification_Data/testline/delo.angles'
+dfI = '/home/deen/Data/MoogStokes/Verification_Data/testline/Belo.spectrum_I'
+dfContinuum = '/home/deen/Data/MoogStokes/Verification_Data/testline/Belo.continuum'
+dfAngles = '/home/deen/Data/MoogStokes/Verification_Data/testline/Belo.angles'
+dfMoog = '/home/deen/Data/MoogStokes/Verification_Data/testline/flux.moog'
 
 Angles = open(dfAngles, 'r')
 StokesI = open(dfI, 'r')
 Continuum = open(dfContinuum, 'r')
+Moog = open(dfMoog, 'r')
+
+moog_wl = []
+moog_fl = []
+
+for line in Moog:
+    l = line.split()
+    if len(l) == 2:
+        moog_wl.append(float(l[0]))
+        moog_fl.append(float(l[1]))
 
 ang_info = []
 
@@ -43,6 +54,12 @@ for line in StokesI:
         a.append(float(fluxes))
     I.append(a)
 
+wave = numpy.mean(wl)
+if ((1.0/(wave/10000.0)) < 2.4):
+   alpha = -0.023 + 0.292/(wave/10000.0)
+else:
+   alpha = -0.507 + 0.441/(wave/10000.0)
+
 I = numpy.array(I)
 I = I.transpose()
 
@@ -59,16 +76,57 @@ C = C.transpose()
 fig = pyplot.figure(1)
 ax = fig.add_subplot(1,1,1)
 
-r2d = 180.0/3.1415926
+r2d = 180.0/pi
+
+final_spectrum = numpy.zeros(len(wl))
+total_weight = 0.0
+
+inclination = pi/2.0
+T_I = numpy.matrix([[1.0, 0.0, 0.0],
+        [0.0, numpy.cos(inclination), numpy.sin(inclination)],
+        [0.0, -numpy.sin(inclination), numpy.cos(inclination)]])
+
+emergent_vector = numpy.matrix([1.0, 0.0, 0.0])
 
 for junk in zip(I, C, ang_info):
+    """
     ax.clear()
     ax.plot(wl, junk[0]/junk[1])
     ax.set_ybound(1.0, 0.4)
     fig.show()
-    mu = junk[2].mu
-    az = junk[2].az
-    longitude = junk[2].longitude
-    daz = -numpy.cos(az[2]) + numpy.cos(az[1])
-    print az[0]*r2d, longitude[0]*r2d, mu
     raw_input()
+    #"""
+    azimuth = junk[2].az
+    n_az_steps = int(azimuth[2]*r2d-azimuth[1]*r2d)
+    azs = azimuth[1]+(numpy.arange(n_az_steps)+0.5)*(azimuth[2]-azimuth[1])/n_az_steps
+    az1 = azimuth[1]+(numpy.arange(n_az_steps))*(azimuth[2]-azimuth[1])/n_az_steps
+    az2 = azimuth[1]+(numpy.arange(n_az_steps)+1.0)*(azimuth[2]-azimuth[1])/n_az_steps
+    longitude = junk[2].longitude
+    dphi = longitude[1]
+    n_phi_steps = int(dphi*r2d)
+    phis = longitude[0]-dphi/2.0+(numpy.arange(n_phi_steps)+0.5)*dphi/n_phi_steps
+    for az in zip(azs, az1, az2):
+        T_rho = numpy.matrix([[0.0, 0.0, 1.0],
+                [-numpy.cos(az[0]), numpy.sin(az[0]), 0.0],
+                [numpy.sin(az[0]), numpy.cos(az[0]), 0.0]])
+        daz = numpy.sin(az[2])-numpy.sin(az[1])
+        area = daz*dphi
+        for phi in phis:
+            T_eta = numpy.matrix([
+                    [numpy.cos(phi), -numpy.sin(phi), 0.0],
+                    [numpy.sin(phi), numpy.cos(phi), 0.0],
+                    [0.0, 0.0, 1.0]])
+            surface_vector = T_I*T_eta*T_rho*emergent_vector.T
+            mu = surface_vector.A[2][0]
+            if (mu > 0.00001):
+                projected_area = area*mu#/(4.0*pi)
+                limb_darkening = (1.0-(1.0-mu**alpha))
+                weight = projected_area*limb_darkening
+                total_weight += weight
+                final_spectrum = final_spectrum + weight*junk[0]/junk[1]
+
+final_spectrum /= total_weight
+
+ax.plot(wl, final_spectrum)
+ax.plot(moog_wl, numpy.array(moog_fl))
+fig.show()
